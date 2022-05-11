10e45279a8b9fec30a4b5a7e698cf1cd20db229f
ben <52321971+Austomos@users.noreply.github.com>
2022-05-11 23:18:39 +0200

    test: add provider test file

---
+++ test/src/Provider/ChasterAppTest.php
@@ -0,0 +1,10 @@
+<?php
+/** @noinspection PhpIllegalPsrClassPathInspection */
+namespace Austomos\OAuth2\Client\Test\Provider;
+
+use PHPUnit\Framework\TestCase;
+
+class ChasterAppTest extends TestCase
+{
+
+}
