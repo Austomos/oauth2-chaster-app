7465a6367bc092eef2ffcb5709805ff1ca8d2b35
ben <52321971+Austomos@users.noreply.github.com>
2022-05-16 01:00:40 +0200

    test: add test of check response and response to cover 100%

--- test/src/Provider/ChasterAppTest.php
+++ test/src/Provider/ChasterAppTest.php
@@ -5,6 +5,9 @@
 namespace Austomos\OAuth2\Client\Test\Provider;
 
 use Austomos\OAuth2\Client\Provider\ChasterApp;
+use Austomos\OAuth2\Client\Provider\ChasterAppResourceOwner;
+use Exception;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Tool\QueryBuilderTrait;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
@@ -39,11 +42,92 @@ class ChasterAppTest extends TestCase
     /**
      * @throws \League\OAuth2\Client\Provider\Exception\IdentityProviderException
      */
+    public function testChasterAppUrls(): void
+    {
+        /** @noinspection PhpUndefinedFieldInspection */
+        $this->provider->oAuthBaseUrl = 'https://sso.chaster.app/auth/realms/app/protocol/openid-connect';
+
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $response->expects('getBody')
+            ->times(1)
+            ->andReturns(
+                http_build_query([
+                    'access_token' => 'mock_access_token',
+                    'expires_in' => 300,
+                    'refresh_expires_in' => 1800,
+                    'refresh_token' => 'mock_refresh_token',
+                    'token_type' => 'bearer',
+                    'not-before-policy' => 0,
+                    'scope' => 'profile'
+                ])
+            );
+        $response->allows('getHeader')
+            ->andReturns(['content-type' => 'application/x-www-form-urlencoded']);
+        $response->allows('getStatusCode')
+            ->andReturns(200);
+
+        $client = m::mock('GuzzleHttp\ClientInterface');
+        $client->expects('send')->times(1)->andReturns($response);
+        /** @noinspection PhpParamsInspection */
+        $this->provider->setHttpClient($client);
+
+        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+
+        $providerAuthorizationUrl = $this->provider->oAuthBaseUrl . '/auth';
+        $providerAccessTokenUrl = $this->provider->oAuthBaseUrl . '/token';
+        $providerResourceOwnerUrl = 'https://api.chaster.app/auth/profile';
+
+        $this->assertEquals($providerAuthorizationUrl, $this->provider->getBaseAuthorizationUrl());
+        $this->assertEquals($providerAccessTokenUrl, $this->provider->getBaseAccessTokenUrl([]));
+        $this->assertEquals($providerResourceOwnerUrl, $this->provider->getResourceOwnerDetailsUrl($token));
+    }
+
+    /**
+     * @throws \League\OAuth2\Client\Provider\Exception\IdentityProviderException
+     * @throws \JsonException
+     */
+    public function testCheckResponseThrowIdentityProviderException(): void
+    {
+        try {
+            $status = random_int(400, 600);
+        } catch (Exception $e) {
+            $status = 500;
+        }
+        $postResponse = m::mock('Psr\Http\Message\ResponseInterface');
+        $postResponse->allows('getBody')
+            ->andReturns(
+                json_encode([
+                    'message' => 'Validation Failed',
+                    'errors' => [
+                        ['resource' => 'Issue', 'field' => 'title', 'code' => 'missing_field'],
+                    ],
+                ], JSON_THROW_ON_ERROR)
+            );
+        $postResponse->allows('getHeader')
+            ->andReturns(['content-type' => 'json']);
+        $postResponse->allows('getStatusCode')
+            ->andReturns($status);
+
+        $client = m::mock('GuzzleHttp\ClientInterface');
+        $client->expects('send')
+            ->times(1)
+            ->andReturns($postResponse);
+        /** @noinspection PhpParamsInspection */
+        $this->provider->setHttpClient($client);
+
+        $this->expectException(IdentityProviderException::class);
+
+        $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+    }
+
+    /**
+     * @throws \League\OAuth2\Client\Provider\Exception\IdentityProviderException
+     */
     public function testGetAccessToken(): void
     {
         $response = m::mock('Psr\Http\Message\ResponseInterface');
         $response->allows('getBody')
-            ->andReturns('{"access_token":"mock_access_token", "scope":"repo,gist", "token_type":"bearer"}');
+            ->andReturns('{"access_token":"mock_access_token", "scope":"profile", "token_type":"bearer"}');
         $response->allows('getHeader')
             ->andReturns(['content-type' => 'json']);
         $response->allows('getStatusCode')
@@ -81,42 +165,87 @@ class ChasterAppTest extends TestCase
     }
 
     /**
+     * @throws \JsonException
      * @throws \League\OAuth2\Client\Provider\Exception\IdentityProviderException
      */
-    public function testGithubEnterpriseDomainUrls(): void
+    public function testProfileData(): void
     {
-        /** @noinspection PhpUndefinedFieldInspection */
-        $this->provider->oAuthBaseUrl = 'https://sso.chaster.app/auth/realms/app/protocol/openid-connect';
+        try {
+            $userId = random_int(1000, 9999);
+        } catch (Exception $e) {
+            $userId = 999;
+        }
+        $username = uniqid('', true);
+        $email = uniqid('', true);
+        $mockString = 'mock_string_value';
+        $mockInt = 123;
+        $mockArray = ['a' => 'A', 'b' => 'B', 'c' => 'C'];
 
-        $response = m::mock('Psr\Http\Message\ResponseInterface');
-        $response->expects('getBody')
-            ->times(1)
+        $postResponse = m::mock('Psr\Http\Message\ResponseInterface');
+        $postResponse->allows('getBody')
             ->andReturns(
                 http_build_query([
                     'access_token' => 'mock_access_token',
-                    'expires' => 3600,
+                    'expires_in' => 300,
+                    'refresh_expires_in' => 1800,
                     'refresh_token' => 'mock_refresh_token',
+                    'token_type' => 'bearer',
+                    'not-before-policy' => 0,
+                    'scope' => 'profile'
                 ])
             );
-        $response->allows('getHeader')
+        $postResponse->allows('getHeader')
             ->andReturns(['content-type' => 'application/x-www-form-urlencoded']);
-        $response->allows('getStatusCode')
+        $postResponse->allows('getStatusCode')
+            ->andReturns(200);
+
+        $userResponse = m::mock('Psr\Http\Message\ResponseInterface');
+        $userResponse->allows('getBody')
+            ->andReturns(
+                json_encode([
+                    '_id' => $userId,
+                    'username' => $username,
+                    'email' => $email,
+                    'mock_string' => $mockString,
+                    'mock_int' => $mockInt,
+                    'mock_array' => $mockArray,
+                ], JSON_THROW_ON_ERROR)
+            );
+        $userResponse->allows('getHeader')
+            ->andReturns(['content-type' => 'json']);
+        $userResponse->allows('getStatusCode')
             ->andReturns(200);
 
         $client = m::mock('GuzzleHttp\ClientInterface');
-        $client->expects('send')->times(1)->andReturns($response);
+        $client->expects('send')
+            ->times(2)
+            ->andReturns($postResponse, $userResponse);
         /** @noinspection PhpParamsInspection */
         $this->provider->setHttpClient($client);
 
         $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+        $profile = $this->provider->getResourceOwner($token);
 
-        $providerAuthorizationUrl = $this->provider->oAuthBaseUrl . '/auth';
-        $providerAccessTokenUrl = $this->provider->oAuthBaseUrl . '/token';
-        $providerResourceOwnerUrl = 'https://api.chaster.app/auth/profile';
+        $this->assertInstanceOf(ChasterAppResourceOwner::class, $profile);
+        $this->assertEquals($userId, $profile->getId());
+        $this->assertEquals($userId, $profile->toArray()['_id']);
+        $this->assertEquals($userId, $profile->_id);
 
-        $this->assertEquals($providerAuthorizationUrl, $this->provider->getBaseAuthorizationUrl());
-        $this->assertEquals($providerAccessTokenUrl, $this->provider->getBaseAccessTokenUrl([]));
-        $this->assertEquals($providerResourceOwnerUrl, $this->provider->getResourceOwnerDetailsUrl($token));
+        $this->assertEquals($username, $profile->getUsername());
+        $this->assertEquals($username, $profile->toArray()['username']);
+        $this->assertEquals($username, $profile->username);
+
+        $this->assertEquals($email, $profile->getEmail());
+        $this->assertEquals($email, $profile->toArray()['email']);
+        $this->assertEquals($email, $profile->email);
+
+        $this->assertEquals($mockString, $profile->toArray()['mock_string']);
+        $this->assertEquals($mockString, $profile->mock_string);
+        $this->assertEquals($mockInt, $profile->toArray()['mock_int']);
+        $this->assertEquals($mockInt, $profile->mock_int);
+        $this->assertEquals($mockArray, $profile->toArray()['mock_array']);
+        $this->assertEquals($mockArray, $profile->mock_array);
+        $this->assertNull($profile->mock_null);
     }
 
     public function testScopes(): void
